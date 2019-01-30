2e755d5cef86f1584a90c6dec41a32484bb8c530
Zeucny <deniscrenn@hotmail.fr>
2019-01-30 16:57:07 +0100

    first commit lol

--- souchoteque/app/Http/Controllers/HomeController.php
+++ souchoteque/app/Http/Controllers/HomeController.php
@@ -6,8 +6,14 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\DB;
 
-class Controller extends BaseController
+class HomeController extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function show(){
+        $results = DB::table('home')->select('*')->get();
+        var_dump($results);
+        return view('home');
+    }
 }
