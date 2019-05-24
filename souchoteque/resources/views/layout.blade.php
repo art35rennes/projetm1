571fdaaa06a86b5d37e8c51867e69b59ec51b8dc
Arthur Sicard <arthur2rennes@hotmail.fr>
2019-05-24 16:11:16 +0200

    a

--- souchoteque/resources/views/layout.blade.php
+++ souchoteque/resources/views/layout.blade.php
@@ -16,7 +16,7 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light mb-3">
         <a class="navbar-brand font-italic pr-2 border-right" href="/home">
             <img src="{{asset('ressources/image/logo.jpg')}}" width="40" height="40" >
-            Souchotèque
+            Souchothèque
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
